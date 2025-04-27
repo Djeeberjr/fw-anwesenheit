84a4e8dbae4070eb0987766bfffd7d95e95df72c
Niklas Kapelle <niklas@kapelle.org>
2025-04-27 15:51:44 +0200

    added a simple pm3 mock script for testing
    use the PM3_BIN env var to use it

---
+++ pm3_mock.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+generate_random_hex() {
+  openssl rand -hex 4 | tr '[:lower:]' '[:upper:]'
+}
+
+echo "Proxmark 3 mock script"
+echo "Outputs a random ID every 1 to 5 seconds"
+
+while true; do
+  random_id=$(generate_random_hex)
+  echo "[+] UID.... $random_id"
+  if (( RANDOM % 2 == 0 )); then
+    echo "[+] UID.... $random_id"
+  fi
+  sleep "$((RANDOM % 5 + 1))"
+done
