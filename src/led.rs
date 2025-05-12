b4a50ba136db3503cd92fe91929c0eaeff886cf4
PC_WSL <philipp.senfft@gmail.com>
2025-05-12 15:30:49 +0200

    fixed timings for LED

--- src/led.rs
+++ src/led.rs
@@ -11,7 +11,7 @@ pub struct Led {
     controller: Ws2812<Spi>,
 }
 
-const STATUS_DURATION: Duration = Duration::from_secs(2); // 2s sleep for all status led signals
+const STATUS_DURATION: Duration = Duration::from_secs(1); // 1s sleep for all status led signals
 
 impl Led {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
@@ -22,6 +22,13 @@ impl Led {
 
     pub async fn turn_green_on_1s(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         self.controller.write(NamedColor::Green.into_iter())?;
+        sleep(STATUS_DURATION).await;
+        self.controller.write(NamedColor::Off.into_iter())?;
+        Ok(())
+    }
+
+    pub async fn turn_red_on_1s(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.controller.write(NamedColor::Red.into_iter())?;
         sleep(STATUS_DURATION).await;
         self.controller.write(NamedColor::Off.into_iter())?;
         Ok(())
