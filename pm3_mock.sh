31f65261df3c4c61967edb18360f123f0c4f6e64
Djeeberjr <djeeberjr@gmail.com>
2025-05-13 12:01:52 +0200

    pm3_mock.sh output special ID on SIGUSR1
    for testing the hotspot

--- pm3_mock.sh
+++ pm3_mock.sh
@@ -4,6 +4,12 @@ generate_random_hex() {
   openssl rand -hex 4 | tr '[:lower:]' '[:upper:]'
 }
 
+output_hotspot_id() {
+  echo "[+] UID.... C1532B57"
+}
+
+trap 'output_hotspot_id' SIGUSR1
+
 echo "Proxmark 3 mock script"
 echo "Outputs a random ID every 1 to 5 seconds"
 
