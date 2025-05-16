bccf019c11f9f2946123bf2a2c59d45a1408c316
Djeeberjr <djeeberjr@gmail.com>
2025-05-16 17:48:04 +0200

    simple display for id mappings in frontend

---
+++ web/src/idMapping.ts
@@ -0,0 +1,45 @@
+export interface IdMapping {
+  id_map: IDMap
+}
+
+export interface IDMap {
+  [name: string]: Name
+}
+
+export interface Name {
+  first: string,
+  last: string,
+}
+
+export async function requestMappings(): Promise<IdMapping> {
+  let req = await fetch("/api/mapping");
+
+  if (req.status != 200) {
+    console.error(await req.text());
+    return { id_map: {} };
+  }
+
+  let mappings: IdMapping = await req.json();
+
+  return mappings;
+}
+
+export async function addMapping(id: string, firstName: string, lastName: string) {
+  let req = await fetch("/api/mapping", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    },
+    body: JSON.stringify({
+      id,
+      name: {
+        first: firstName,
+        lastName: lastName,
+      },
+    })
+  });
+
+  if (req.status != 200){
+    console.error(await req.text())
+  }
+}
