141c1aa9cb58f4d2deb5af4445bf902326366509
Djeeberjr <djeeberjr@gmail.com>
2025-10-20 14:29:09 +0200

    added downloadBlob function

---
+++ web/src/lib/downloadBlob.ts
@@ -0,0 +1,8 @@
+export function downloadBlob(filename: string, content: string, mimeType = "text/plain") {
+  const blob = new Blob([content], { type: mimeType });
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(a.href);
+}
