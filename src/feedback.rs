e5f81f15bf28a41a00e5130ccf597501f9356a1d
Djeeberjr <djeeberjr@gmail.com>
2026-09-21 12:01:02 +0200

    added new FeedbackState
    also refactored file

--- src/feedback.rs
+++ src/feedback.rs
@@ -1,10 +1,10 @@
 use embassy_time::{Duration, Timer};
 use esp_hal::gpio::Output;
-use esp_hal_smartled::SmartLedsAdapterAsync;
-use log::debug;
+use esp_hal_smartled::{LedAdapterError, SmartLedsAdapterAsync};
+use log::{debug, error};
 use smart_leds::SmartLedsWriteAsync;
+use smart_leds::brightness;
 use smart_leds::colors::{BLACK, GREEN, RED, YELLOW};
-use smart_leds::{brightness, colors::BLUE};
 
 use crate::init::hardware;
 use crate::{FEEDBACK_STATE, init};
@@ -15,8 +15,7 @@ pub enum FeedbackState {
     Nack,
     Error,
     Startup,
-    WIFI,
-    Idle,
+    SilentAck,
 }
 
 const LED_LEVEL: u8 = 255;
@@ -31,105 +30,81 @@ pub async fn feedback_task(
     debug!("Starting feedback task");
     loop {
         let feedback_state = FEEDBACK_STATE.wait().await;
-        match feedback_state {
-            FeedbackState::Ack => {
-                led.write(brightness(
-                    [GREEN; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(100)).await;
-                buzzer.set_low();
-                Timer::after(Duration::from_millis(50)).await;
-                led.write(brightness(
-                    [BLACK; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-            }
-            FeedbackState::Nack => {
-                led.write(brightness(
-                    [YELLOW; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(100)).await;
-                buzzer.set_low();
-                Timer::after(Duration::from_millis(100)).await;
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(100)).await;
-                buzzer.set_low();
-                led.write(brightness(
-                    [BLACK; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-            }
-            FeedbackState::Error => {
-                led.write(brightness(
-                    [RED; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(500)).await;
-                buzzer.set_low();
-                Timer::after(Duration::from_millis(500)).await;
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(500)).await;
-                buzzer.set_low();
-            }
-            FeedbackState::Startup => {
-                led.write(brightness(
-                    [GREEN; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(10)).await;
-                buzzer.set_low();
-                Timer::after(Duration::from_millis(10)).await;
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(10)).await;
-                buzzer.set_low();
-                Timer::after(Duration::from_millis(50)).await;
-                buzzer.set_high();
-                Timer::after(Duration::from_millis(100)).await;
-                buzzer.set_low();
-
-                Timer::after(Duration::from_secs(2)).await;
-                led.write(brightness(
-                    [BLACK; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-            }
-            FeedbackState::WIFI => {
-                led.write(brightness(
-                    [BLUE; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-            }
-            FeedbackState::Idle => {
-                led.write(brightness(
-                    [BLACK; init::hardware::NUM_LEDS].into_iter(),
-                    LED_LEVEL,
-                ))
-                .await
-                .unwrap();
-            }
-        };
-        debug!("Feedback state: {:?}", feedback_state);
+        if let Err(err) = display_state(feedback_state, &mut led, &mut buzzer).await {
+            error!("Failed to displat feedback state: {:?}", err);
+        }
     }
+}
+
+async fn led_set(
+    led: &mut SmartLedsAdapterAsync<'static, { hardware::LED_BUFFER_SIZE }>,
+    color: smart_leds::RGB8,
+) -> Result<(), LedAdapterError> {
+    led.write(brightness(
+        [color; init::hardware::NUM_LEDS].into_iter(),
+        LED_LEVEL,
+    ))
+    .await
+}
+
+async fn display_state(
+    state: FeedbackState,
+    led: &mut SmartLedsAdapterAsync<'static, { hardware::LED_BUFFER_SIZE }>,
+    buzzer: &mut Output<'static>,
+) -> Result<(), LedAdapterError> {
+    match state {
+        FeedbackState::Ack => {
+            led_set(led, GREEN).await?;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(100)).await;
+            buzzer.set_low();
+            Timer::after(Duration::from_millis(50)).await;
+            led_set(led, BLACK).await?;
+        }
+        FeedbackState::Nack => {
+            led_set(led, YELLOW).await?;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(100)).await;
+            buzzer.set_low();
+            Timer::after(Duration::from_millis(100)).await;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(100)).await;
+            buzzer.set_low();
+            led_set(led, BLACK).await?;
+        }
+        FeedbackState::Error => {
+            led_set(led, RED).await?;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(500)).await;
+            buzzer.set_low();
+            Timer::after(Duration::from_millis(500)).await;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(500)).await;
+            buzzer.set_low();
+        }
+        FeedbackState::Startup => {
+            led_set(led, GREEN).await?;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(10)).await;
+            buzzer.set_low();
+            Timer::after(Duration::from_millis(10)).await;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(10)).await;
+            buzzer.set_low();
+            Timer::after(Duration::from_millis(50)).await;
+            buzzer.set_high();
+            Timer::after(Duration::from_millis(100)).await;
+            buzzer.set_low();
+
+            Timer::after(Duration::from_secs(2)).await;
+            led_set(led, BLACK).await?;
+        }
+        FeedbackState::SilentAck => {
+            led_set(led, GREEN).await?;
+            Timer::after(Duration::from_millis(150)).await;
+            led_set(led, BLACK).await?;
+        }
+    };
+
+    Ok(())
 }
